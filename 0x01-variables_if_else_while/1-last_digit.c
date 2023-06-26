9f82862b775fa714f3ef1e3f09442fa18da1fd1e
Confidence93-O <74902792+Confidence93-O@users.noreply.github.com>
2023-06-26 13:42:03 +0200

    Update 1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -7,15 +7,13 @@
  * and whether it is greater than 5, less than 6, or 0.
  *
  * Return: Always 0.
- */ 
-
+ */
 int main(void)
 {       
         int n, lastd;
-	
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	lastd =n % 10;
+	lastd = n % 10;
 	
 	if (lastd > 5)
 	{
