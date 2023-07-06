df2b37ce8bd35fee2847387b648091428c77b738
Confidence93-O <Fhatunditwane@gmail.com>
2023-07-06 12:28:38 -0700

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -4,7 +4,7 @@
 int _sqrt(int n, int i);
 
 /**
- * _sqrt_recursion _returns the natursl sqaure root of a number
+ * _sqrt_recursion - Returns the natursl sqaure root of a number
  * @n: number to calculate the natural square root
  *
  * Return: the natural square root
